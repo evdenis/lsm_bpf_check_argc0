18e2cfa17428b31db16d384b3a1e33d25b070f51
Denis Efremov <denis.e.efremov@oracle.com>
2022-02-10 00:31:59 +0300

    Check check_argc0_lsm__attach() result
    Signed-off-by: Denis Efremov <denis.e.efremov@oracle.com>

--- load_check_argc0_lsm.c
+++ load_check_argc0_lsm.c
@@ -1,9 +1,30 @@
 #include "check_argc0_lsm.h"
 #include <unistd.h>
 
+static void check_fail(bool ok, const char *fmt, ...)
+{
+	va_list ap;
+
+	if (ok)
+		return;
+
+	va_start(ap, fmt);
+	fprintf(stderr, fmt, ap);
+	va_end(ap);
+	fprintf(stderr, "\n");
+
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
-	check_argc0_lsm__attach(check_argc0_lsm__open_and_load());
+	struct check_argc0_lsm *lsm;
+
+	lsm = check_argc0_lsm__open_and_load();
+	check_fail(lsm, "failed to load bpf");
+
+	check_argc0_lsm__attach(lsm);
+
 	for(;;) {
 		sleep(1);
 	}
